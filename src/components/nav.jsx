608c56f8eca68d27e08bca7e4304dfcdca8212cf
Caitlin Wang <caitlinwang97@gmail.com>
2018-06-27 12:38:26 -0400

    created basic nav bar and added bootstrap container class to site content

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,9 +3,26 @@ import Link from 'gatsby-link';
 
 const Nav = ({ siteTitle }) => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">
-      This will be our Navbar
-    </a>
+    <Link className="navbar-brand" to="/home/">
+      {siteTitle}
+    </Link>
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/stories/">
+          Stories
+        </Link>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/sponsors/">
+          Sponsors
+        </a>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/projects/">
+          Projects
+        </Link>
+      </li>
+    </ul>
   </nav>
 );
 
