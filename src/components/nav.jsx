be93cb2ec75ca86631fbda515b4254f17bcd7984
Caitlin <caitlinwang97@gmail.com>
2018-06-28 14:18:14 -0400

    changed path to home page

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,7 +4,7 @@ import Link from 'gatsby-link';
 const Nav = ({ siteTitle }) => (
   // TODO arrange links for mobile/smaller screens
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/home/">
+    <Link className="navbar-brand" to="/">
       {siteTitle}
     </Link>
     <ul className="navbar-nav mr-auto">
