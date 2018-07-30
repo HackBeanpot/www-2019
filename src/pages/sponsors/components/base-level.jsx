b0968ac992c8474d46096b09de40b74e3c002c86
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2018-07-30 17:31:34 -0400

    Sponsorship page style

---
+++ src/pages/sponsors/components/base-level.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+
+class BaseLevel extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="">
+              <div className="sponsor-subtitle">Base Level</div>
+              <ul>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet</li>
+              </ul>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default BaseLevel;
