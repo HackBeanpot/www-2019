6da29106787429a72bb5602c887c9c5c8b4187f6
Luciana Corteggiano <lucianacorteggiano@Luci.local>
2018-08-29 11:28:58 -0400

    Style sponsor logos responsively

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FAQItems from 'components/faq-items';
 import MailingListPrompt from 'components/MailingListPrompt';
+import SponsorLogos from 'components/sponsor-logos';
 import AboutItem from 'components/aboutItem/AboutItem';
 import FAQs from 'data/FAQs.json';
 import AboutContent from 'data/aboutContent.json';
@@ -9,7 +10,7 @@ const App = () => {
   return (
     <div className="light-background">
       <section className="landing-section container">
-        <div class="row">
+        <div className="row">
           <img
             className="hero-logo col-4"
             src="https://hackbeanpot.com/img/solid_logo.png"
@@ -56,59 +57,8 @@ const App = () => {
         <FAQItems FAQs={FAQs} />
       </section>
 
-      <section className="sponsors-section">
-        <h2>2018 Sponsors</h2>
-        <div className="row">
-          <a href="https://www.intuit.com/" className="sponsor col-12 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/Intuit_2016_RGB.jpg" />
-          </a>
-        </div>
-        <div className="row">
-          <a href="https://wearegenuine.com/" className="sponsor col-6 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/genuine-840.png" />
-          </a>
-          <a href="http://www.jackmorton.com/" className="sponsor col-6 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/jack-morton-logo.jpg" />
-          </a>
-        </div>
-        <div className="row">
-          <a href="https://marketing.poweradvocate.com/" className="sponsor col-12 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/PowerAdvocate-500.png" />
-          </a>
-        </div>
-        <div className="row">
-          <a href="https://www.trinetx.com/" className="sponsor col-12 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/TriNetX-450.jpg" />
-          </a>
-        </div>
-        <div className="row">
-          <a href="https://www.northeastern.edu/pan/" className="sponsor col-4 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/pan-neu-500.jpg" />
-          </a>
-          <a href="https://drift.com//" className="sponsor col-4 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/DriftLogo-300.png" />
-          </a>
-          <a href="https://www.bookbub.com/careers" className="sponsor col-4 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/bookbub-428.png" />
-          </a>
-        </div>
-        <div className="row">
-          <a href="http://www.gtiproperties.com/" className="sponsor col-6 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/GTI_logo-840.png" />
-          </a>
-          <a href="http://www.roughdraft.vc/" className="sponsor col-6 text-center">
-            <img src="https://hackbeanpot.com/img/sponsors/rdv-logo-150.png" />
-          </a>
-        </div>
-        <h2>In-kind sponsors</h2>
-        <div className="row">
-          <a href="http://unionsquaredonuts.com/" className="sponsor col-6 text-center">
-            <img src="https://hackbeanpot.com/img/in-kind-sponsors/union-square-donuts-700.png" />
-          </a>
-          <a href="https://www.chipotle.com/" className="sponsor col-6 text-center">
-            <img src="https://hackbeanpot.com/img/in-kind-sponsors/Chipotle-Full-Logo.png" />
-          </a>
-        </div>
+      <section className="sponsors-section container">
+        <SponsorLogos />
       </section>
     </div>
   );
