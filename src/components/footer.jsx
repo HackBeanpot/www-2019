e3fee20529a976445cca5541a61a533c88875f83
Caitlin Wang <caitlinwang97@gmail.com>
2019-11-03 18:30:10 -0500

    Add leaves

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,17 @@ import DynamicLink from './dynamic-link';
 import SocialLinks from 'data/social-links.json';
 
 import LogoWhite from 'images/logo-white.jsx';
+import FooterLeaves1 from 'images/svg/footer-leaves-1';
+import FooterLeaves2 from 'images/svg/footer-leaves-2';
 
 const Footer = () => (
   <div className="footer">
+    <span className="footer__leaves-left">
+      <FooterLeaves1 />
+    </span>
+    <span className="footer__leaves-right">
+      <FooterLeaves2 />
+    </span>
     <div className="footer__logo">
       <LogoWhite />
     </div>
