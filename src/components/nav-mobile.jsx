b4a22af40b75ca9a328374dc68bc97cdc68eaaf7
Caitlin Wang <caitlinwang97@gmail.com>
2019-11-03 22:14:58 -0500

    Make mobile nav functional component

--- src/components/nav-mobile.jsx
+++ src/components/nav-mobile.jsx
@@ -1,66 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import DynamicLink from './dynamic-link.jsx';
 import NavLinks from 'data/nav-links.json';
 
 import LogoIcon from 'images/svg/logo-icon.jsx';
 import LogoIconWhite from 'images/svg/logo-icon-white.jsx';
 
-class NavMobile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isNavOpen: false
-    };
+const NavMobile = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-    this.menuToggle = this.menuToggle.bind(this);
+  function menuToggle() {
+    setIsNavOpen(!isNavOpen);
   }
 
-  menuToggle() {
-    this.setState({ isNavOpen: !this.state.isNavOpen });
-  }
-
-  render() {
-    return (
-      <nav
-        className={`nav-mobile ${this.state.isNavOpen ? 'opened' : 'closed'}`}
+  return (
+    <nav className={`nav-mobile ${isNavOpen ? 'opened' : 'closed'}`}>
+      <div className="nav-mobile__top">
+        <DynamicLink to="/" className="nav__logo">
+          {isNavOpen ? <LogoIconWhite /> : <LogoIcon />}
+        </DynamicLink>
+        <button onClick={menuToggle} className="nav-mobile__toggle-btn">
+          <i
+            className={
+              isNavOpen
+                ? 'mdi mdi-24px mdi-close close-icon'
+                : 'mdi mdi-24px mdi-menu menu-icon'
+            }
+          />
+        </button>
+      </div>
+      <ul
+        className="nav-mobile__list"
+        style={isNavOpen ? { display: 'block' } : { display: 'none' }}
       >
-        <div className="nav-mobile__top">
-          <DynamicLink to="/" className="nav__logo">
-            {this.state.isNavOpen ? <LogoIconWhite /> : <LogoIcon />}
-          </DynamicLink>
-          <button onClick={this.menuToggle} className="nav-mobile__toggle-btn">
-            <i
-              className={
-                this.state.isNavOpen
-                  ? 'mdi mdi-24px mdi-close close-icon'
-                  : 'mdi mdi-24px mdi-menu menu-icon'
-              }
-            />
-          </button>
-        </div>
-        <ul
-          className="nav-mobile__list"
-          style={
-            this.state.isNavOpen ? { display: 'block' } : { display: 'none' }
-          }
-        >
-          {NavLinks.map((link, index) => {
-            return (
-              <li key={`nav-item-${index}`} className="nav-mobile__item">
-                <DynamicLink
-                  className="nav-mobile__link"
-                  to={link.path}
-                  onClick={this.menuToggle}
-                >
-                  {link.name}
-                </DynamicLink>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    );
-  }
-}
+        {NavLinks.map((link, index) => {
+          return (
+            <li key={`nav-item-${index}`} className="nav-mobile__item">
+              <DynamicLink
+                className="nav-mobile__link"
+                to={link.path}
+                onClick={menuToggle}
+              >
+                {link.name}
+              </DynamicLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 export default NavMobile;
