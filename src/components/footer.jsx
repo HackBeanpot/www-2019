b9a72fb5a34cc619cd25f4a84650411b8002a17c
Catu <catu@Catus-MacBook-Pro.local>
2020-07-30 22:08:50 -0400

    footer update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DynamicLink from './dynamic-link';
 import SocialLinks from 'data/social-links.json';
 
@@ -10,42 +10,18 @@ import addToMailchimp from 'gatsby-plugin-mailchimp';
 
 const Footer = () => {
   // state stuff goes here 
-  state = {
-    name: null,
-    email: null,
-}
+  const [email, setEmail] = useState('');
 
-_handleChange = e => {
-  console.log({
-      [`${e.target.name}`]: e.target.value,
-  })
-  this.setState({
-      [`${e.target.name}`]: e.target.value,
-  })
-}
-
-_handleSubmit = e => {
-  e.preventDefault()
-
-  console.log('submit', this.state)
-  addToMailchimp(this.state.email, this.state)
-  .then(({ msg, result }) => {
-      console.log('msg', `${result}: ${msg}`)
-
-      if (result !== 'success') {
-          throw msg
-      }
-      alert(msg)
-  })
-  .catch(err => {
-      console.log('err', err)
-      alert(err)
-  })
-
-} 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
 
-
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value) //for testing
+    setEmail(e.target.value)
+    }
       
 
 
@@ -78,24 +54,23 @@ _handleSubmit = e => {
       </DynamicLink>
     </div>
     <div>
-    <form onSubmit={this._handleSubmit}>
-                        <input
-                            type="text"
-                            onChange={this._handleChange}
-                            placeholder="name"
-                            name="name"
-                        />
-                        <input
-                            type="email"
-                            onChange={this._handleChange}
-                            placeholder="email"
-                            name="email"
-                        />
-                        <br />
-                        <br />
+    <p>Sign up for our newsletter</p>
 
-                        <input type="submit" />
-                    </form>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          placeholder="Email address"
+          name="email"
+          type="text"
+          onChange={handleEmailChange}
+        />
+
+    <div class="clear footer-cta-padding">
+    <button className="footer__cta" type="submit">Subscribe</button>
+
+    </div>
+      </div>
+    </form>
     </div>
   </div>
   
