d79e6fe802c6581262d3eb15bac33a10f58692a2
daniaalnahdi <alnahdi.d@husky.neu.edu>
2020-09-20 00:54:24 +0300

    add error/success msgs to newsletter signup

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import addToMailChimp from 'gatsby-plugin-mailchimp';
+
 import DynamicLink from './dynamic-link';
 import SocialLinks from 'data/social-links.json';
 
@@ -6,89 +8,87 @@ import LogoWhite from 'images/logo-white.jsx';
 import FooterLeaves1 from 'images/svg/footer-leaves-1';
 import FooterLeaves2 from 'images/svg/footer-leaves-2';
 
-import addToMailchimp from 'gatsby-plugin-mailchimp';
-
-
 const Footer = () => {
-  // state stuff goes here 
   const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  const handleSubmit = (e) => {
-    console.log(e.target.value) //for testing
-    addToMailchimp(email) 
-    .then(data => {
-      // I recommend setting data to React state
-      // but you can do whatever you want (including ignoring this `then()` altogether)
-      console.log(data)
-    })
-    .catch(() => {
-      // unnecessary because Mailchimp only ever
-      // returns a 200 status code
-      // see below for how to handle errors
-    })
-   e.preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const { result, msg } = await addToMailChimp(email);
+
+      if (result !== 'success') {
+        throw msg; //error with subscription from our end or plugin
+      }
+
+      setEmail('');
+      setMessage('Thank you for subscribing!');
+    } catch (err) {
+      if (err.includes('already subscribed')) {
+        setMessage("You're already subscribed!");
+      } else {
+        setMessage('Please enter a valid email.');
+      }
+    }
   };
 
-
-  const handleEmailChange = (e) => {
+  const handleEmailChange = e => {
     e.preventDefault();
-    console.log(e.target.value) //for testing
-    setEmail(e.target.value)
-    }
-      
+    setEmail(e.target.value);
+  };
 
-
-  return(
- <div className="footer">
-    <span className="footer__leaves-left">
-      <FooterLeaves1 />
-    </span>
-    <span className="footer__leaves-right">
-      <FooterLeaves2 />
-    </span>
-    <div className="footer__logo">
-      <LogoWhite />
-    </div>
-    <div className="footer__content">
-      <ul className="footer__social">
-        {SocialLinks.map((item, index) => {
-          return (
-            <li key={`social-link-${index}`}>
-              <DynamicLink to={item.link} className="footer__social-link">
-                <i className={`mdi mdi-36px ${item.iconClass}`} />
-              </DynamicLink>
-            </li>
-          );
-        })}
-      </ul>
-      <p>HackBeanpot, Inc. is a registered 501(c)(3) organization.&nbsp;</p>
-      <DynamicLink className="footer__coc-link" to="/code-of-conduct">
-        Code of Conduct
-      </DynamicLink>
-    </div>
-    <div>
-    <p>Sign up for our newsletter</p>
-
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input
-          placeholder="Email address"
-          name="email"
-          type="text"
-          onChange={handleEmailChange}
-        />
-
-    <div class="clear footer-cta-padding">
-    <button className="footer__cta" type="submit">Subscribe</button>
-
-    </div>
+  return (
+    <div className="footer">
+      <span className="footer__leaves-left">
+        <FooterLeaves1 />
+      </span>
+      <span className="footer__leaves-right">
+        <FooterLeaves2 />
+      </span>
+      <div className="footer__logo">
+        <LogoWhite />
       </div>
-    </form>
-    </div>
-  </div>
-  
-  )
-  }
+      <div className="footer__content">
+        <ul className="footer__social">
+          {SocialLinks.map((item, index) => {
+            return (
+              <li key={`social-link-${index}`}>
+                <DynamicLink to={item.link} className="footer__social-link">
+                  <i className={`mdi mdi-36px ${item.iconClass}`} />
+                </DynamicLink>
+              </li>
+            );
+          })}
+        </ul>
+        <p>HackBeanpot, Inc. is a registered 501(c)(3) organization.&nbsp;</p>
+        <DynamicLink className="footer__coc-link" to="/code-of-conduct">
+          Code of Conduct
+        </DynamicLink>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit} className="footer-form">
+          <div>
+            <div className="footer-form-heading">Sign up for our newsletter</div>
+            <input
+              placeholder="Email address"
+              name="email"
+              type="text"
+              onChange={handleEmailChange}
+              className="footer-form-input"
+            />
+            <div className="footer-form-msg">{message}</div>
 
-  export default Footer;
-  
+            <div class="clear footer-cta-padding">
+              <button className="footer__cta" type="submit">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
